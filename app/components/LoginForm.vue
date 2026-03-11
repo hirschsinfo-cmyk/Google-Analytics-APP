72a0b515bf946a6ac414b42b36cd1f3b41fe4445
ClintonKode <clinton@elikode.co.za>
2026-03-11 16:56:25 +0200

    Authentication Layer

--- app/components/LoginForm.vue
+++ app/components/LoginForm.vue
@@ -113,7 +113,7 @@ const handleLogin = async () => {
   loading.value = true
 
   try {
-    const response = await fetch('http://localhost:3001/login', {
+    const response = await fetch('https://ga-hirsch-frontend-1.onrender.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
