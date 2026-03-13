33f1f0368f3b35d500ecf6cf711b0025b8a03dd0
ClintonKode <clinton@elikode.co.za>
2026-03-13 11:44:39 +0200

    corrected api bath

--- app/components/LoginForm.vue
+++ app/components/LoginForm.vue
@@ -113,7 +113,7 @@ const handleLogin = async () => {
   loading.value = true
 
   try {
-    const response = await fetch('https://ga-hirsch-frontend-1.onrender.com/login', {
+    const response = await fetch('https://google-analytics-api-1.onrender.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
